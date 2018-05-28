6a86e0c82fed37f490313ce6ed9d0cb2b6598044
fhgajardo <fhgajardo@uc.cl>
2018-05-28 02:17:02 -0400

    first try

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,16 @@
+class WelcomeController < ApplicationController
+
+	require 'rubygems'
+	require 'websocket-client-simple'
+
+
+	# Protocolo: wss://
+    # Servidor: integracion-tarea-3.herokuapp.com
+	# Ruta: /flights
+
+  	def index
+
+ 	end
+
+
+end
