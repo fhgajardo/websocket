0c4080d6537eca470139a7c9d56b2606c8495af3
fhgajardo <fhgajardo@uc.cl>
2018-05-29 04:29:48 -0400

    v1

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,49 +10,15 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-var ws = new WebSocket("wss://integracion-tarea-3.herokuapp.com/flights");
-
-ws.onmessage = function(e){
-  print(e.data);
-};
-
-ws.onopen = function(e){
-  //ws.send("FLIGHTS")
-
-  log("websocket open");
-  console.log(e);
-};
-
-ws.onclose = function(e){
-  log("websocket close");
-  console.log(e);
-};
-
-var log = function(msg){
-  console.log(msg);
-  $("#chat").prepend($("<li>").text("[log] "+msg));
-};
-
-$(function(){
-  $("#btn_post").click(post);
-  $("#message").keydown(function(e){
-    if(e.keyCode == 13) post();
-  });
-});
-
-var post = function(){
-  var name = $("#name").val();
-  var mes = $("#message").val();
-  ws.send(name+" : "+mes);
-  $("input#message").val("");
-};
+//= require socket.io
 
 
 
-var print = function(msg){
-  $("#chat").prepend($("<li>").text(msg));
-};
+
+
